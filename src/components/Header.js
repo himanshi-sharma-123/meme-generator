2c41376fae39c8c24bfcb2fa508eb5509c957f71
Himanshi Sharma <himanshish456@gmail.com>
2023-04-23 19:47:28 +0530

    meme generator

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import trollface from "../images/troll-face.png"
+export default function Header(){
+    return(
+        <header className="header">
+            <img src={trollface} alt="" className="header--image"/>
+              <h2 className="header--title">Meme Generator</h2>
+              <h4 className="header--project">React Course - Project3</h4>
+        </header>
+        
+    )
+}

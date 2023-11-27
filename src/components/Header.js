3c44b130dc7427ae28d90e20c1332c69466df612
Himanshi Sharma <himanshish456@gmail.com>
2023-11-27 13:08:21 +0530

    small issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
-import trollface from "../images/troll-face.png"
-export default function Header(){
-    return(
-        <header className="header">
-            <img src={trollface} alt="" className="header--image"/>
-              <h2 className="header--title">Meme Generator</h2>
-              <h4 className="header--project">React Course - Project3</h4>
-        </header>
-        
-    )
+import trollface from "../images/troll-face.png";
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={trollface} alt="" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React.js</h4>
+    </header>
+  );
 }
